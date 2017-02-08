e578d652d9a04349d1847c2af01de931c5217a84
Sifan YE <sye8@u.rochester.edu>
2017-02-07 23:12:30 -0500

    Add files via upload

---
+++ Combinations.java
@@ -0,0 +1,27 @@
+/*
+ *Name: SIFAN,YE
+ *NetID: sye8
+ *Project 1
+ *Lab Section: M/W 1400-1515 Lab TA: Chengyu (cdeng2)
+ *Collaborator: Ziyu Song (zsong10), Qieyue Sun ()
+ *Combinations of colors
+ */
+
+public class Combinations {
+	
+	//Those are indexed from 0 - 7
+	public static Mastermind.Colors[] possibleColors
+	= {Mastermind.Colors.RED, 
+	   Mastermind.Colors.BLUE, 
+	   Mastermind.Colors.YELLOW, 
+	   Mastermind.Colors.GREEN, 
+	   Mastermind.Colors.CYAN,
+	   Mastermind.Colors.PURPLE, 
+	   Mastermind.Colors.ORANGE, 
+	   Mastermind.Colors.GREY};
+	
+	public static int[] fourFromSixSet = new int[(int) Math.pow(6, 4)];
+	
+	
+	
+}
