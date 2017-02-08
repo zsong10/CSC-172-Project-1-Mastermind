f38c991514878fcba66333f75d99af52fdf1cb5b
Sifan YE <sye8@u.rochester.edu>
2017-02-07 23:08:42 -0500

    Mastermind Java Class, shell

---
+++ Mastermind.java
@@ -0,0 +1,35 @@
+/*
+ *Name: SIFAN,YE
+ *NetID: sye8
+ *Project 1
+ *Lab Section: M/W 1400-1515 Lab TA: Chengyu (cdeng2)
+ *Collaborator: Ziyu Song (zsong10), Qieyue Sun ()
+ *Main Mastermind class
+ */
+
+public class Mastermind {
+	
+	public enum Colors{
+		RED, BLUE, YELLOW, GREEN, CYAN, PURPLE, ORANGE, GREY;
+	}
+
+	public Mastermind(String[] tokencolors, int positions) {
+	   
+	}
+	
+	public void response(int colorsRightPositionWrong, int positionsAndColorRight){
+		
+	}
+	   
+	//Reset the game
+	public void newGame(){
+		
+	} 
+	
+	// return the next guess
+	public String [] nextMove(){
+		
+	}  
+
+	
+}
